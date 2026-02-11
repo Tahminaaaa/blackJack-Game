d8acbabfee57b7812b4f953edef1fcf26244f762
Tahmina <tahminahagverdiyevaa@gmail.com>
2026-02-11 15:47:40 +0100

    new cards to the array

--- index.js
+++ index.js
@@ -14,10 +14,7 @@ function startGame() {
 };
 
 function renderGame() {
-    cardsEl.textContent = "Cards: ";
-    for (let i = 0; i < cards.length; i++) {
-        cardsEl.textContent += cards[i] + " "
-    }
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
     sumEl.textContent = "Sum: " + sum;
     if (sum <= 20) {
         message = "Do you want to draw a new card?";
